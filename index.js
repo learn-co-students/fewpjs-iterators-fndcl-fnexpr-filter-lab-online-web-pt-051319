a16b2f0f65570a7fabe8072c65647f31ce236617
Jason McGloin <jason.mcgloin@gmail.com>
2019-11-25 00:17:57 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+const findMatching = (drivers, name) => {
+	return drivers.filter( driver => driver.match(RegExp(name, 'gi')) )
+}
+
+const fuzzyMatch = (drivers, name) => {
+	return drivers.filter(driver => driver.match(RegExp(`\\b${name}`, 'gi')))
+}
+
+const matchName = (drivers, name) => {
+	return drivers.filter(driver => driver.name == name)
+}
