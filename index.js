17956c77d7dc81b67248e053cb89f2818e5a09a9
ashok997 <ashok997@gmail.com>
2019-12-28 14:55:50 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+//['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, str) {
+    let result = drivers.filter(d => d.toUpperCase() === str.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(drivers, str) {
+    let result = drivers.filter(d => d.charAt(0) === str.charAt(0));
+    return result;
+}
+
+function matchName(drivers, str) {
+    let result = drivers.filter(d => d.name.toUpperCase() === str.toUpperCase());
+    return result;
+}
+
